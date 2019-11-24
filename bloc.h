b767dee92b3e5d2f6a77d525b535d82446937ba4
Ericlpa <cireslin@hotmail.fr>
2019-11-24 16:30:50 +0100

    bloc

--- bloc.h
+++ bloc.h
@@ -1,15 +1,28 @@
 #ifndef BLOC_H_INCLUDED
 #define BLOC_H_INCLUDED
+#include<vector>
+#include<string>
 
-class bloc
+class Bloc
 {
 private :
+    double m_largeur;
+    double m_hauteur;
+    double m_x;
+    double m_y;
     std::string m_nom;
     std::string m_couleur;
-    std::string<bloc*> m_enfants;
+    std::vector<Bloc*> m_bloc_enfant;
+    Bloc* m_conteneur;
+
 
 public :
-    bloc(std::string nom,std::string couleur);
+    Bloc(double largeur,double hauteur,double x,double y,std::string nom,std::string couleur);
+    Bloc();
+    ~Bloc();
+    void ajouterbloc();
+
+
 };
 
 
